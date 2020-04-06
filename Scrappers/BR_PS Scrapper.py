514cb52d2ddec844fa808030af76002b56b0a9a6
Pipe Galera <pipegalera@gmail.com>
2020-04-06 10:47:43 +0200

    Updated

--- Scrappers/BR_PS Scrapper.py
+++ Scrappers/BR_PS Scrapper.py
@@ -1,3 +1,11 @@
+# @Author: Pipe galera
+# @Date:   05-04-2020
+# @Email:  pipegalera@gmail.com
+# @Last modified by:   Pipe galera
+# @Last modified time: 06-04-2020
+
+
+
 """
 title: "NBA Scrapers"
 author: "Pipe Galera"
@@ -9,17 +17,24 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+import os
+os.chdir("/Users/fgm.si/Documents/GitHub/Optimizing-NBA-Player-Selection")
+# Scraping the Datasets
 
-#######################
-# Advanced Statistics #
-#######################
+###################################
+#   Basketball reference data     #
+###################################
+
+# Advanced Statistics
 
 url_list = []
-year = range(2001,2019)
+year = range(2001,2021)
 
-for i in 2019: # Choose the year
+for i in year:
     url = "https://www.basketball-reference.com/leagues/NBA_{}_advanced.html".format(i)
     url_list.append(url)
+
+url_list
 
 # Opening up connection, grabbing the page
 html = urlopen(url)
@@ -37,7 +52,7 @@ headers.remove('\xa0')
 headers.remove('\xa0')
 headers.remove('Rk')
 headers = ['Team' if x == 'Tm' else x for x in headers]
-headers
+
 
 # Getting the body
 rows = soup.findAll('tr')[1:]
@@ -47,12 +62,12 @@ adv_stats = pd.DataFrame(stats).dropna()
 
 adv_stats = adv_stats.drop([18, 23], axis = 1)
 adv_stats.columns = headers
+adv_stats
 
-###################################
 #  Statistics per 100 posessions  #
-###################################
 
-for i in 2019:
+
+for i in year:
     url = 'https://www.basketball-reference.com/leagues/NBA_{}_per_poss.html'.format(i)
     url_list.append(url)
 
@@ -74,28 +89,25 @@ stats.columns = headers
 stats = stats.drop(columns = ['', 'Age', 'G', 'MP'],axis = 1)
 
 """
-Test: The following boolean should be True before merging
+Test: The following boolean should be True before merging:
 stats['Player'].loc[565] == adv_stats['Player'].loc[565]
+Try other numbers
 """
-
-all_stats = pd.merge(stats, adv_stats, on = 'Player')
-all_stats['Team_x'] == all_stats['Team_y']
-all_stats.shape
-all_stats = all_stats[all_stats['Team_x'] == all_stats['Team_y']].reset_index(drop = True)
+stats
+all_stats = pd.merge(stats, adv_stats, on = ["Player", "Team", "Pos"])
 all_stats.shape
 
 ###################################
 #  Salaries                       #
 ###################################
 """
-DISCLAIMER: Basketball reference update the webpage of contracts, so I have used the
-archive of the webpage
+DISCLAIMER: Basketball reference update the webpage of contracts, so I have used the archive of the webpage
 """
 # Getting headers and removing incorrect named columns
-url = "https://web.archive.org/web/20181214210832/https://www.basketball-reference.com/contracts/players.html"
+url = "https://web.archive.org/web/20200229105705/https://www.basketball-reference.com/contracts/players.html"
 html = urlopen(url)
 soup = BeautifulSoup(html)
-headers = ['Player', 'Team', 'Salary 2018-19', 'Salary 2019-20', 'Salary 2020-21', 'Salary 2021-22', 'Salary 2022-23', 'Salary 2023-24', 'Signed Using', 'Guaranteed']
+headers = ['Player', 'Team', 'Salary 2019-20', 'Salary 2020-21', 'Salary 2021-22', 'Salary 2022-23', 'Salary 2023-24', 'Salary 2023-2024', 'Signed Using', 'Guaranteed']
 
 # Getting the body
 rows = soup.findAll('tr')[1:]
@@ -105,16 +117,19 @@ salaries = pd.DataFrame(salaries, columns = headers).dropna()
 salaries = salaries.apply(lambda x: x.str.replace('$', ''))
 salaries = salaries.apply(lambda x: x.str.replace(',', ''))
 
-# Cleaning missing data
-cleaning = ['Salary 2018-19', 'Salary 2019-20', 'Salary 2020-21', 'Salary 2021-22', 'Salary 2022-23', 'Salary 2023-24', 'Guaranteed']
 
-for i in cleaning:
+# Cleaning missing data
+salaries_list = ['Salary 2019-20', 'Salary 2020-21', 'Salary 2021-22', 'Salary 2022-23', 'Salary 2023-24', 'Salary 2023-2024', 'Guaranteed']
+
+for i in salaries_list:
     salaries[i] = salaries[i].replace('', 0)
 salaries['Signed Using'] = salaries['Signed Using'].replace('', 'NaN')
 
 # Changing the type from objects to integers
-for i in cleaning:
+salaries.dtypes
+for i in salaries_list:
     salaries[i] = salaries[i].astype('int')
+
 """
 Agregating the contracts within the same team.
 salaries = salaries.groupby(['Player', 'Team', 'Guaranteed'], as_index = False ).sum()
@@ -122,55 +137,86 @@ salaries.loc[salaries.Player == "Dwight Howard"]
 salaries = salaries.groupby(['Player', 'Team', 'Guaranteed'], as_index = False ).sum()
 salaries.loc[salaries.Player == "Dwight Howard"]
 """
+
 #########################################
 #  Merging Salaries with all the stats  #
 #########################################
 
-data_players = pd.merge(all_stats, salaries, on = 'Player')
+salaries
+data_players = pd.merge(all_stats, salaries, on = ['Player', 'Team'])
 data_players.reset_index(drop = True, inplace = True)
+data_players
 
-# 'Hiring players to play' assumption
+#########################
+#  Tidying the dataset  #
+#########################
+
+# Creating a dictionary to map the name of the teams
+
+teams_dict = {
+'CLE': 'Cleveland Cavaliers',
+'TOR': 'Toronto Raptors',
+'WAS': 'Washington Wizards',
+'BOS': 'Boston Celtics',
+'CHI': 'Chicago Bulls',
+'MIA': 'Miami Heat',
+'IND': 'Indiana Pacers',
+'BRK': 'Brooklyn Nets',
+'CHO': 'Charlotte Hornets',
+'ORL': 'Orlando Magic',
+'NYK': 'New York Knicks',
+'MIL': 'Milwaukee Bucks',
+'ATL': 'Atlanta Hawks',
+'DET': 'Detroit Pistons',
+'PHI': 'Philadelphia 76ers',
+'DAL': 'Dallas Mavericks',
+'DEN': 'Denver Nuggets',
+'GSW': 'Golden State Warriors',
+'HOU': 'Houston Rockets',
+'LAC': 'Los Angeles Clippers',
+'LAL': 'Los Angeles Lakers',
+'MEM': 'Memphis Grizzlies',
+'MIN': 'Minnesota Timberwolves',
+'NOP': 'New Orleans Pelicans',
+'OKC': 'Oklahoma City Thunder',
+'PHO': 'Phoenix Suns',
+'POR': 'Portland Trail Blazers',
+'SAC': 'Sacramento Kings',
+'SAS': 'San Antonio Spurs',
+'UTA': 'Utah Jazz'
+}
+
+
+data_players['Team'] = data_players['Team'].map(teams_dict)
+data_players
 data_players.columns
-data_players = data_players[data_players['Team_x'] == data_players['Team']]
-data_players = data_players[data_players['Pos_x'] == data_players['Pos_y']]
 
-data_players.shape
-data_players = data_players.drop(columns = ['Team_y', 'Pos_y', 'Team'])
-data_players.columns
-data_players = data_players.rename(columns = {'Team_x': 'Team','Pos_x': 'Position'})
-sort_names = ['CLE','TOR', 'WAS', 'BOS', 'CHI', 'MIA', 'IND', 'BRK', 'CHO', 'ORL', 'NYK', 'MIL',
-'ATL', 'DET', 'PHI', 'DAL', 'DEN', 'GSW', 'HOU', 'LAC', 'LAL', 'MEM', 'MIN', 'NOP', 'OKC', 'PHO',
-'POR', 'SAC', 'SAS', 'UTA']
-teams = ['Cleveland Cavaliers','Toronto Raptors', 'Washington Wizards', 'Boston Celtics',
-'Chicago Bulls', 'Miami Heat', 'Indiana Pacers', 'Brooklyn Nets', 'Charlotte Hornets',
-'Orlando Magic', 'New York Knicks', 'Milwaukee Bucks', 'Atlanta Hawks', 'Detroit Pistons',
-'Philadelphia 76ers', 'Dallas Mavericks', 'Denver Nuggets', 'Golden State Warriors',
-'Houston Rockets', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies',
-'Minnesota Timberwolves', 'New Orleans Pelicans', 'Oklahoma City Thunder', 'Phoenix Suns',
-'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Utah Jazz']
+# Clearning data types
 
-data_players['Team'] = data_players['Team'].replace(sort_names, teams)
-data_players.reset_index(drop = True)
-data_players.columns
-data_players.to_csv('/Users/mac/GitHub/Optimizing-NBA-Player-Selection/Datasets/BR_players_data.csv', index = False)
-data_players = pd.read_csv('/Users/mac/GitHub/Optimizing-NBA-Player-Selection/Datasets/BR_players_data.csv')
+data_players.dtypes
+data_players.iloc[:,3:-5] = data_players.iloc[:,3:-5].apply(pd.to_numeric)
+data_players.dtypes
 
+#############################################################
+#  Creating columns for first, second unit, and third unit  #
+#############################################################
 
-################################################
-#  Creating columns for first and second unit  #
-################################################
+fu = pd.DataFrame(columns= data_players.columns, dtype = "float64")
+su = pd.DataFrame(columns= data_players.columns, dtype = "float64")
+tu = pd.DataFrame(columns= data_players.columns, dtype = "float64")
 
-list_teams = data_players.Team.unique().tolist()
-fu = pd.DataFrame()
-su = pd.DataFrame()
-for i in list_teams:
-    fu = fu.append(data_players.loc[data_players['Team'] == '{}'.format(i)].nlargest(5,'MP'))
-    fu['Role'] = 'FU'
-    su = su.append(data_players.loc[data_players['Team'] == '{}'.format(i)].nlargest(5,'MP').drop(data_players.loc[data_players['Team'] == '{}'.format(i)].nlargest(10, 'MP').index[:5]))
-    su['Role'] = 'SU'
+for key, value in teams_dict.items():
+    fu = fu.append(data_players.loc[data_players['Team'] == '{}'.format(value)].nlargest(5,'MP'))
+    su = su.append(data_players.loc[data_players['Team'] == '{}'.format(value)].nlargest(10, 'MP')[5:])
+    tu = tu.append(data_players.loc[data_players['Team'] == '{}'.format(value)].nlargest(20, 'MP')[11:])
+
+fu["Role"] = "First Unit"
+su["Role"] = "Second Unit"
+tu["Role"] = "Third Unit"
+
+# Check
+data_players.loc[data_players['Team'] == '{}'.format('Miami Heat')].nlargest(20,'MP')[["Player", "MP", "Role"]]
 
 
-data_optimization = pd.concat([fu,su])
-data_optimization.to_csv('/Users/mac/GitHub/Optimizing-NBA-Player-Selection/Datasets/BR_players_data_optimization.csv', index = False )
-
-data_players.loc[data_players['Team'] == '{}'.format('Miami Heat')].nlargest(6,'MP')
+data_players = pd.concat([fu,su, tu])
+data_players.to_csv('out_data/players_data.csv', index = False)
